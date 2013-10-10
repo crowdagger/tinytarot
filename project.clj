5938b44103551e4d5e2573c7a31402e254831982
Elisabeth Henry <liz.henry@ouvaton.org>
2013-10-10 20:47:12 +0200

    project setup

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tinytarot "0.1.0-SNAPSHOT"
+  :description "TiNy Tarot, a french tarot Game"
+  :url "http://tnt.ouvaton.org"
+  :license {:name "GNU General Public License 2.0"
+            :url "http://www.gnu.org/licenses/gpl-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
